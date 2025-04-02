16e697cfbcc2aca2db719734f9235f1cfcf17646
hossein <hosseinbakhtiari781@outlook.com>
2025-04-02 18:17:52 +0330

    Dynamic Search & Pagination of the posts (backend)

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -149,73 +149,131 @@ const getPost = async (req, res, next) => {
   }
 };
 
+// const getAllPosts = async (req, res, next) => {
+//   // try {
+//   //   const filter = req.query.searchKeyword;
+//   //   const categories = req.query.categories
+//   //     ? req.query.categories.split(",")
+//   //     : []; // Expecting categories to be comma-seperated
+
+//   //   let where = {};
+
+//   //   if (filter) {
+//   //     where.title = { $regex: filter, $options: "i" };
+//   //   }
+
+//   //   if (categories.length > 0) {
+//   //     where.categories = { $in: categories };
+//   //   }
+
+//   //   let query = Post.find(where);
+//   //   const page = parseInt(req.query.page) || 1;
+//   //   const pageSize = parseInt(req.query.limit) || 10;
+//   //   const skip = (page - 1) * pageSize;
+//   //   const total = await Post.find(where).countDocuments();
+//   //   const pages = Math.ceil(total / pageSize);
+
+//   //   res.header({
+//   //     "x-filter": filter,
+//   //     "x-totalcount": JSON.stringify(total),
+//   //     "x-currentpage": JSON.stringify(page),
+//   //     "x-pagesize": JSON.stringify(pageSize),
+//   //     "x-totalpagecount": JSON.stringify(pages),
+//   //   });
+
+//   //   if (page > pages) {
+//   //     return res.json([]);
+//   //   }
+
+//   //   const result = await query
+//   //     .skip(skip)
+//   //     .limit(pageSize)
+//   //     .populate([
+//   //       {
+//   //         path: "user",
+//   //         select: ["avatar", "name", "verified"],
+//   //       },
+//   //       {
+//   //         path: "categories",
+//   //         select: ["title"],
+//   //       },
+//   //     ])
+//   //     .sort({ updatedAt: "desc" });
+
+//   //   return res.json(result);
+//   // } catch (error) {
+//   //   next(error);
+//   // }
+//   try {
+//     const posts = await Post.find({}).populate([
+//       {
+//         path: "user",
+//         select: ["avatar", "name", "verified"],
+//       },
+//     ]);
+
+//     res.json(posts);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
 const getAllPosts = async (req, res, next) => {
-  // try {
-  //   const filter = req.query.searchKeyword;
-  //   const categories = req.query.categories
-  //     ? req.query.categories.split(",")
-  //     : []; // Expecting categories to be comma-seperated
-
-  //   let where = {};
-
-  //   if (filter) {
-  //     where.title = { $regex: filter, $options: "i" };
-  //   }
-
-  //   if (categories.length > 0) {
-  //     where.categories = { $in: categories };
-  //   }
-
-  //   let query = Post.find(where);
-  //   const page = parseInt(req.query.page) || 1;
-  //   const pageSize = parseInt(req.query.limit) || 10;
-  //   const skip = (page - 1) * pageSize;
-  //   const total = await Post.find(where).countDocuments();
-  //   const pages = Math.ceil(total / pageSize);
-
-  //   res.header({
-  //     "x-filter": filter,
-  //     "x-totalcount": JSON.stringify(total),
-  //     "x-currentpage": JSON.stringify(page),
-  //     "x-pagesize": JSON.stringify(pageSize),
-  //     "x-totalpagecount": JSON.stringify(pages),
-  //   });
-
-  //   if (page > pages) {
-  //     return res.json([]);
-  //   }
-
-  //   const result = await query
-  //     .skip(skip)
-  //     .limit(pageSize)
-  //     .populate([
-  //       {
-  //         path: "user",
-  //         select: ["avatar", "name", "verified"],
-  //       },
-  //       {
-  //         path: "categories",
-  //         select: ["title"],
-  //       },
-  //     ])
-  //     .sort({ updatedAt: "desc" });
-
-  //   return res.json(result);
-  // } catch (error) {
-  //   next(error);
-  // }
   try {
-    const posts = await Post.find({}).populate([
-      {
-        path: "user",
-        select: ["avatar", "name", "verified"],
-      },
-    ]);
+    const filter = req.query.searchKeyword;
+    // const categories = req.query.categories
+    //   ? req.query.categories.split(",")
+    //   : []; // Expecting categories to be comma-seperated
 
-    res.json(posts);
+    let where = {};
+
+    if (filter) {
+      where.title = { $regex: filter, $options: "i" };
+    }
+
+    // if (categories.length > 0) {
+    //   where.categories = { $in: categories };
+    // }
+
+    let query = Post.find(where);
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * pageSize;
+    const total = await Post.find(where).countDocuments();
+    const pages = Math.ceil(total / pageSize);
+
+    res.header({
+      "x-filter": filter,
+      "x-totalcount": JSON.stringify(total),
+      "x-currentpage": JSON.stringify(page),
+      "x-pagesize": JSON.stringify(pageSize),
+      "x-totalpagecount": JSON.stringify(pages),
+    });
+
+    if (page > pages) {
+      return res.json([]);
+    }
+
+    const result = await query
+      .skip(skip)
+      .limit(pageSize)
+      .populate([
+        {
+          path: "user",
+          select: ["avatar", "name", "verified"],
+        },
+        // {
+        //   path: "categories",
+        //   select: ["title"],
+        // },
+      ])
+      .sort({ updatedAt: "desc" });
+
+    return res.json(result);
   } catch (error) {
     next(error);
   }
 };
+
 
 export { createPost, updatePost, deletePost, getPost, getAllPosts };
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
 const router = express.Router();
-import { createComment, updateComment } from "../controllers/commentControllers.js";
+import {
+  createComment,
+  deleteComment,
+  updateComment,
+} from "../controllers/commentControllers.js";
 import { authGuard } from "../middleware/authMiddleware.js";
 
 router.post("/", authGuard, createComment);
-router.put("/:commentId", authGuard, updateComment);
+router
+  .route("/:commentId")
+  .put(authGuard, updateComment)
+  .delete(authGuard, deleteComment);
 export default router;
